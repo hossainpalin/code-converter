ae1e4d7e7fde2583579c38937289935dd9bfb0bf
Hossain Muhammad Palin <engineer.palin@gmail.com>
2025-02-07 14:00:47 +0600

    Initial commit

---
+++ commitlint.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ["@commitlint/cli", "@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "pref",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+      ],
+    ],
+    "subject-case": [0, "never"],
+  },
+};
